e605d5eed8e3d6d2aebbf4ed5370540130a562d3
Philippe Z Lin <philippe_z_lin@trend.com.tw>
2015-12-14 03:05:22 +0000

    Parse UTF8 strings

--- fetchroots.py
+++ fetchroots.py
@@ -23,17 +23,25 @@ if r.status_code == 200:
 # Importantly, these are not PEM formatted certs but base64 encoded
 # ASN.1 (DER) encoded
 
+def utf8(s):
+    if s is not None:
+        return s.encode('UTF-8')
+    else:
+        return None
+
 for i in roots:
    certs = roots[i]
    for k in certs:
        try:
            certobj = crypto.load_certificate(crypto.FILETYPE_ASN1,base64.b64decode(k))
            subject = certobj.get_subject()
-           print 'CN={},OU={},O={},L={},S={},C={}'.format(subject.commonName,
-                                                      subject.organizationalUnitName,
-                                                      subject.organizationName,
-                                                      subject.localityName,
-                                                      subject.stateOrProvinceName,
-                                                      subject.countryName)
+           print 'CN={},OU={},O={},L={},S={},C={}'.format(
+                   utf8(subject.commonName),
+                   utf8(subject.organizationalUnitName),
+                   utf8(subject.organizationName),
+                   utf8(subject.localityName),
+                   utf8(subject.stateOrProvinceName),
+                   subject.countryName)
        except:
            print subject.get_components()
+           raise
